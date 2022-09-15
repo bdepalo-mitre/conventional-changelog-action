5b92132408625b326108d234b0a6fce276d7be08
Brandon DePalo <bdepalo@mitre.org>
2022-09-15 13:55:20 -0400

    rework bump logic

--- src/helpers/bumpVersion.js
+++ src/helpers/bumpVersion.js
@@ -12,137 +12,139 @@ const requireScript = require('./requireScript')
  * @returns {string}
  */
 module.exports = async (releaseType, lastVersion, lastRelease) => {
-  let major, minor, patch, normalTag, prereleaseTag, prereleasePrefix, prereleaseVersion
+  let major, minor, patch, prereleaseVersion, newVersion;
 
-  let inputPrereleasePrefix = core.getInput('prerelease')
+  let inputPrereleasePrefix = core.getInput('prerelease');
 
-  core.info(`Previous release: ${lastRelease}`)
+  // get the previous release
+  if (lastRelease){
 
-  if (lastVersion) {
-    [normalTag, prereleaseTag] = lastVersion.split('-');
-    [major, minor, patch] = normalTag.split('.');
+    // last release exists
+    core.info(`Previous release: ${lastRelease}`);
+    let releaseMajor, releaseMinor, releasePatch;
+    let previousMajor, previousMinor, previousPatch;
+    let previousBaseVersion, previousPrerelease, previousPrereleasePrefix, previousPrereleaseVersion;
 
-    if (prereleaseTag) {
-      [prereleasePrefix, prereleaseVersion] = prereleaseTag.split('.');
-      prereleaseVersion = parseInt(prereleaseVersion, 10);
+    // split up release version
+    [releaseMajor, releaseMinor, releasePatch] = lastRelease.split('.');
+    releaseMajor = parseInt(releaseMajor, 10);
+    releaseMinor = parseInt(releaseMinor, 10);
+    releasePatch = parseInt(releasePatch, 10);
+
+    if (lastVersion) {
+
+      [previousBaseVersion, previousPrerelease] = lastVersion.split('-');
+
+      // split up previous prerelease version (if present)
+      if (previousPrerelease) {
+        // previous version was a prerelease
+        [previousPrereleasePrefix, previousPrereleaseVersion] = previousPrerelease.split('.');
+        previousPrereleaseVersion = parseInt(previousPrereleaseVersion, 10);
+      }
+
+      // split up previous version (should always be present)
+      if (previousBaseVersion) {
+        [previousMajor, previousMinor, previousPatch] = lastRelease.split('.');
+        previousMajor = parseInt(previousMajor, 10);
+        previousMinor = parseInt(previousMinor, 10);
+        previousPatch = parseInt(previousPatch, 10);
+      }
     }
 
-    major = parseInt(major, 10)
-    minor = parseInt(minor, 10)
-    patch = parseInt(patch, 10)
+    // calculate base version
+    switch (releaseType) {
+      case 'major':
+        major = releaseMajor + 1;
+        minor = 0;
+        patch = 0;
+        break;
+
+      case 'minor':
+        major = releaseMajor;
+        minor = releaseMinor + 1;
+        patch = 0;
+        break;
+
+      default:
+        major = releaseMajor;
+        minor = releaseMinor;
+        patch = releasePatch + 1;
+    }
+
 
     if (inputPrereleasePrefix) {
+      // this is a prerelease
+      prereleaseVersion = 0; // default
 
-      // prerelease with previous version
-      core.info(`Pre-release with previous version: ${lastVersion}`)
-
-      if (prereleaseTag) {
-        // previous version was a prerelease TODO: complicated
-        prereleaseVersion = prereleaseVersion + 1
-      } else {
-
-        // previous version was a release
-        prereleaseVersion = 0
-
-        switch (releaseType) {
-          case 'major':
-            major = major + 1
-            minor = 0
-            patch = 0
-            break
-
-          case 'minor':
-            minor = minor + 1
-            patch = 0
-            break
-
-          default:
-            patch = patch + 1
+      if (previousPrereleaseVersion) {
+        // previous version was a prerelease with version
+        if (inputPrereleasePrefix === previousPrereleasePrefix) {
+          // previous prerelease has the same prefix
+          if (previousMajor === major && previousMinor === minor && previousPatch === patch) {
+            // release type didn't change from previous prerelease, increment prerelease version
+            prereleaseVersion = previousPrereleaseVersion + 1;
+          }
         }
       }
+
+      newVersion = `${major}.${minor}.${patch}-${inputPrereleasePrefix}.${prereleaseVersion}`;
 
     } else {
-
-      // releases with version
-      core.info(`Release with previous version: ${lastVersion}`)
-
-      if (prereleaseTag) {
-        // previous version was a prerelease TODO: complicated
-        prereleaseVersion = prereleaseVersion + 1
-      } else {
-        // previous version was a release
-
-        switch (releaseType) {
-          case 'major':
-            major = major + 1
-            minor = 0
-            patch = 0
-            break
-
-          case 'minor':
-            minor = minor + 1
-            patch = 0
-            break
-
-          default:
-            patch = patch + 1
-        }
-      }
+      // this is a release
+      newVersion = `${major}.${minor}.${patch}`;
     }
+
   } else {
 
-    // anything with no version
+    // last release does not exist, use fallback
+    let fallback = semverValid(core.getInput('fallback-version'));
 
-    let version = semverValid(core.getInput('fallback-version'))
-    prereleaseVersion = 0;
+    if (fallback) {
 
-    if (version) {
-      [major, minor, patch] = version.split('.')
+      // split up fallback
+      [major, minor, patch] = fallback.split('.');
+      major = parseInt(major, 10);
+      minor = parseInt(minor, 10);
+      patch = parseInt(patch, 10);
+
     } else {
+
       // default
-      major = 0
-      minor = 1
-      patch = 0
+      major = 0;
+      minor = 1;
+      patch = 0;
+
     }
 
-    core.info(`The previous version could not be detected, using fallback '${major}.${minor}.${patch}'.`)
-  }
+    core.info(`The previous release could not be detected, using fallback '${major}.${minor}.${patch}'.`);
 
-  const preChangelogGenerationFile = core.getInput('pre-changelog-generation')
+    if (inputPrereleasePrefix) {
+      // this is a prerelease
+      newVersion = `${major}.${minor}.${patch}-${inputPrereleasePrefix}.0`;
 
-  let newVersion;
-
-  if(inputPrereleasePrefix) {
-
-    // special case where prerelease prefixes change
-    if(prereleaseTag && inputPrereleasePrefix !== prereleasePrefix){
-      core.debug(`Prerelease prefix has changed from ${prereleasePrefix} to ${inputPrereleasePrefix}`)
-      prereleaseVersion = 0
+    } else {
+      // this is a release
+      newVersion = `${major}.${minor}.${patch}`;
     }
-
-    // prerelease
-    newVersion = `${major}.${minor}.${patch}-${inputPrereleasePrefix}.${prereleaseVersion}`
-  } else {
-
-    // release
-    newVersion = `${major}.${minor}.${patch}`
   }
 
-  core.info(`Unmodified new version: ${newVersion}`)
+  core.info(`Unmodified new version: ${newVersion}`);
+
+  const preChangelogGenerationFile = core.getInput('pre-changelog-generation');
 
   if (preChangelogGenerationFile) {
-    const preChangelogGenerationScript = requireScript(preChangelogGenerationFile)
+    const preChangelogGenerationScript = requireScript(preChangelogGenerationFile);
 
     // Double check if we want to update / do something with the version
     if (preChangelogGenerationScript && preChangelogGenerationScript.preVersionGeneration) {
-      const modifiedVersion = await preChangelogGenerationScript.preVersionGeneration(newVersion)
+      const modifiedVersion = await preChangelogGenerationScript.preVersionGeneration(newVersion);
 
       if (modifiedVersion) {
-        core.info(`Using modified version "${modifiedVersion}"`)
-        newVersion = modifiedVersion
+        core.info(`Using modified version "${modifiedVersion}"`);
+        newVersion = modifiedVersion;
       }
     }
   }
 
-  return newVersion
+  return newVersion;
 }
